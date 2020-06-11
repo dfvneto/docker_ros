30efc03a809ebf830a2dbad6a3b2adac45bb4c15
Domingos Ferreira Viana Neto <dfvneto@gmail.com>
2020-06-11 19:41:41 -0300

    Update new_view.sh

--- new_view.sh
+++ new_view.sh
@@ -3,6 +3,6 @@ then
     echo "Docker ID not passed in arguments"
 else
     echo "Accessing docker $1"
-    docker exec -it $1 "source /opt/ros/kinetic/setup.bash" bash
+    docker exec -it $1 bash "source /opt/ros/kinetic/setup.bash"
 fi
 
